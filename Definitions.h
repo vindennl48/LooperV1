1e2a8d5152f9dfaebea3c5264e5f17384b1c8b36
Mitchell Woodbine <mitchellwoodbine@gmail.com>
2022-07-04 13:25:01 -0400

    Added definitions file

---
+++ Definitions.h
@@ -0,0 +1,22 @@
+#ifndef DEFINITIONS_H
+#define DEFINITIONS_H
+
+
+// Pin defines
+#define PinButton1  9
+#define PinButton2  10
+
+#define PinSwitch1  11
+#define PinSwitch2  12
+#define PinSwitch3  13
+#define PinSwitch4  14
+
+#define PinLED1_1   15
+#define PinLED1_2   16
+#define PinLED1_3   17
+#define PinLED2_1   18
+#define PinLED2_2   19
+#define PinLED2_3   20
+
+
+#endif
