bb2078a1b6d07c2aca7247a85b1bf252ab420757
Mitchell Woodbine <mitchellwoodbine@gmail.com>
2022-07-29 21:19:46 -0400

    Added ability to turn on/off disabling interrupts
    the 'atomic' function will turn on and off the ability to disable
    interrupts during buffer changes.

--- Ring.h
+++ Ring.h
@@ -11,36 +11,44 @@
 
 template <typename T>
 struct Ring {
-  T *buff_t;
+  T        *buff_t;
   uint16_t size, head, tail;
+  uint8_t  is_atomic;
 
   Ring(uint16_t size) {
     this->size = size;
     buff_t = new T [size];
-    head = tail = 0;
+    head = tail = is_atomic = 0;
   }
   ~Ring() { for (int i=0; i<size; i++) delete &buff_t[i]; }
 
   void insert(T *item) { insert(*item); }
   void insert(T item) {
-    __disable_irq();
+    disable_irq();
     buff_t[head] = item;
     ROTATE(head, size);
     if ( head == tail ) { ROTATE(tail, size); }
-    __enable_irq();
+    enable_irq();
   }
 
   T pop() {
-    __disable_irq();
+    disable_irq();
     if ( head == tail ) { return buff_t[0]; }
     uint16_t result_tail = tail;
     ROTATE(tail, size);
     return buff_t[result_tail];
-    __enable_irq();
+    enable_irq();
   }
 
   bool is_empty() { return head == tail ? true : false; }
   bool is_full()  { return ROTATE_IF(head, size) == tail ? true : false; }
+
+  // true false
+  void atomic(uint8_t is_atomic) { this->is_atomic = is_atomic; }
+
+private:
+  void disable_irq() { if ( is_atomic ) __disable_irq() }
+  void enable_irq()  { if ( is_atomic ) __enable_irq() }
 };
 
 #endif
